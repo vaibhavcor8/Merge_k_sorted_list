a08d53914fa86e240950c4d94aace391f3023b3d
Vaibhav Srivastava <vaibhavsriv@google.com>
2026-02-06 12:09:29 +0530

    fifth

--- Test.py
+++
@@ -1,3 +0,0 @@
-import sys
-print("Python Version:", sys.version)
-print("Success! VS Code is working on gLinux.")
--- main.py
+++
@@ -1,8 +0,0 @@
-num=[1,2,3,4,5]
-even_square=[]
-res=0
-for x in num:
-    if x%2==0:
-        res=x*x
-        even_square.append(res)
-print(even_square)
